ad58505a3bf9379d7cf3fa98e5e9cb17bea3301e
Alfredo Martínez <Nothing-Dev@users.noreply.github.com>
2015-03-08 14:45:06 -0430

    Update common.mk

--- config/common.mk
+++ config/common.mk
@@ -341,4 +341,32 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 -include vendor/cyngn/product.mk
 
+
+# ROM Version
+
+#Versioning
+
+# Export rom name
+export VENDOR := cxr
+
+# Version info
+ROM_VERSION_MAJOR := 2
+ROM_VERSION_MINOR := 0
+ROM_VERSION_MAINTENANCE := 
+ROM_VERSION_TAG := BETA
+
+#CXR_BUILD
+
+ifneq ($ROM_VERSION_TAG,"")
+        VERSION := $(ROM_VERSION_MAJOR).$(ROM_VERSION_MINOR)$(ROM_VERSION_MAINTENANCE)-$(ROM_VERSION_TAG)
+else
+        VERSION := $(ROM_VERSION_MAJOR).$(ROM_VERSION_MINOR)$(ROM_VERSION_MAINTENANCE)
+endif
+
+export ROM_VERSION := $(VERSION)-$(shell date -u +%Y%m%d)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.modversion=$(ROM_VERSION) \
+    ro.cxr.version=$(VERSION)
+
 $(call prepend-product-if-exists, vendor/extra/product.mk)
