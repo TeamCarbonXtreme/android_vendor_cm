f024ecd2db673f60bb826d96ce7bcf999c7fba2c
ROM-PacMe <rafy.pachecomendez@gmail.com>
2015-04-13 20:45:26 -0300

    add mini configs for ARMv6 Devices
    Change-Id: If83e51983d6d5c0bf4a8048e7a329525db22c09f

---
+++ config/mini.mk
@@ -0,0 +1,27 @@
+# Mini config (Required /system size: min=175MB)
+
+# Terminal Emulator
+PRODUCT_COPY_FILES +=  \
+    vendor/cm/proprietary/Term.apk:system/app/Term.apk \
+    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+
+PRODUCT_PACKAGES += \
+    DSPManager \
+    libcyanogen-dsp \
+    audio_effects.conf \
+    Apollo \
+    CMFileManager \
+    Basic \
+    LockClock
+
+PRODUCT_PACKAGES += \
+    CellBroadcastReceiver
+
+PRODUCT_PACKAGES += \
+    Development \
+    Email2 \
+    Exchange2 \
+    SoundRecorder
+
+
+$(call inherit-product, vendor/cm/config/tiny.mk)
